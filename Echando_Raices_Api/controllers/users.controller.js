29a7145eea0273f7b75aa25ed06be7a32746a8cf
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2021-05-06 17:21:31 -0700

    Added Route users/:userId

--- Echando_Raices_Api/controllers/users.controller.js
+++ Echando_Raices_Api/controllers/users.controller.js
@@ -3,18 +3,71 @@ const bcrypt = require('bcrypt');
 const dbPool = require('../db/sqldb');
 
 // GET ALL USERS (EN VERSION FINAL QUITAR PASSWORD)
-exports.users_list = (req, res, next) => {
+exports.users_getAll = (req, res, next) => {
     dbPool.getConnection((err, conn) => {
-        if(err) {
+        if(err)
             res.status(500).json({ error: err });
-        } else {
-            const sql = 'SELECT * FROM usuario';
+        else {
+            const sql = 'SELECT * FROM usuario;';
             conn.query(sql, (error, rows, fields) => {
                 if(!error) {
-                    res.status(200).json(rows);
+                    const response = {
+                        users: rows.map(row => {
+                            return {
+                                _id: row.idusuario,
+                                email: row.email,
+                                name: row.nombre,
+                                last_name: row.primer_apellido,
+                                phone_num: row.telefono,
+                                permissionLevel: row.permiso,
+                                request: {
+                                    type: 'GET',
+                                    url: 'http://' + process.env.API_HOST + ':' + process.env.PORT + '/users/' + row.idusuario
+                                }
+                            }
+                        })
+                    }
+                    res.status(200).json(response);
                 }
             });
         }
         conn.release();
     });
+}
+
+// GET SPECIFIED USER BY ID
+exports.users_getBydId = (req, res, next) => {
+    dbPool.getConnection((err, conn) => {
+        if(err)
+            return res.status(500).json({ error: err });
+        
+        const id = req.params.userId;
+        const sql = 'SELECT * FROM usuario WHERE idusuario = ' + id + ';';
+        conn.query(sql, (error, rows, fields) => {
+            if(!error) {
+                if(rows.length > 0) {
+                    const response = {
+                        user: rows.map(row => {
+                            return {
+                                _id: row.idusuario,
+                                email: row.email,
+                                name: row.nombre,
+                                last_name: row.primer_apellido,
+                                phone_num: row.telefono,
+                                permissionLevel: row.permiso,
+                                request: {
+                                    type: 'GET',
+                                    url: 'http://' + process.env.API_HOST + ':' + process.env.PORT + '/users'
+                                }
+                            }
+                        })[0] // <- so it isn't sent as json array
+                    }
+                    res.status(200).json(response);
+                } else {
+                    res.status(404).json({ message: 'No valid entry for specified ID' });
+                }
+            }
+        });
+    })
+
 }
--- Echando_Raices_Api/routes/users.js
+++ Echando_Raices_Api/routes/users.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const UsersController = require('../controllers/users.controller');
 
 // GET ALL USERS
-router.get('/', UsersController.users_list);
+router.get('/', UsersController.users_getAll);
+
+// GET SPECIFIED USER BY ID
+router.get('/:userId', UsersController.users_getBydId);
 
 module.exports = router;
