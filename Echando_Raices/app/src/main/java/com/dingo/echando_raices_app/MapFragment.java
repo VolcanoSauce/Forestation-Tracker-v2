a5ad0b2feb325ad5326ee76b14b8da35af4e251c
VolcanoSauce <37091802+VolcanoSauce@users.noreply.github.com>
2021-05-18 21:04:19 -0700

    Fetch Map coords test in HomeFragment

--- Echando_Raices/app/src/main/java/com/dingo/echando_raices_app/MapFragment.java
+++ Echando_Raices/app/src/main/java/com/dingo/echando_raices_app/MapFragment.java
@@ -10,11 +10,19 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,21 +34,29 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
 public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback{
     double[] latitudes = {32.532574, 32.546138, 32.509015, 32.530127};
     double[] longitudes = {-116.965011, -116.976379, -116.992868, -117.023896};
     String[] plants = {"UABC", "Aeropuerto", "Hipodromo", "CECUT"};
+    JSONObject forestations;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
 
         assert supportMapFragment != null;
         supportMapFragment.getMapAsync(this);
+
+        forestations = new JSONObject();
+        setForestations(getContext());
 
         return view;
     }
@@ -85,5 +101,28 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerClickList
         Toast.makeText(getContext(), marker.getTitle(), Toast.LENGTH_SHORT).show();
 
         return false;
+    }
+
+    public void setForestations(Context ctx) {
+        String url = "http://10.0.2.2:3600/forestations";
+        //String url = UtilitiesER.getApiBaseUrl() + "forestations";
+        RequestQueue queue = Volley.newRequestQueue(ctx);
+        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                forestations = response;
+                try {
+                    Log.d("MyResponse", forestations.getJSONArray("forestations").getJSONObject(0).getJSONObject("coords").toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("MyErrorResponse", error.toString());
+            }
+        });
+        queue.add(jsonArrayRequest);
     }
 }
